d9306b81d6100e548d9b9da5018166c9d5a10d85
potatmen <ashatokhi2-c@my.cityu.edu.hk>
2023-05-29 18:00:08 +0900

    merged files

--- tester.py
+++ tester.py
@@ -3,6 +3,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
+search_list = ["input", "button"]
+
+def get_elements(soup, name):
+    return soup.find_all(name)
+
+def parsing_elements(filename):
+    with open(filename) as file:
+        html_data = file.read()
+    soup = BeautifulSoup(html_data, 'html.parser')
+    resulted_list = []
+    for el in search_list:
+        objects = get_elements(soup, el)
+        for obj in objects:
+            resulted_list.append((el, obj['id']))
+    return resulted_list
 
 driver = webdriver.Chrome()
 driver.get("http://www.google.com") # change to local host
