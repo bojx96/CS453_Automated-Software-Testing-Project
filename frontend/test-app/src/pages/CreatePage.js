b9276c8a9b2797465c54b98c3d99bc7d63fa6c8b
bojx96 <brandon2_ong@mymail.sutd.edu.sg>
2023-05-29 23:08:22 +0800

    feat: populated createpage

--- frontend/test-app/src/pages/CreatePage.js
+++ frontend/test-app/src/pages/CreatePage.js
@@ -1,4 +1,22 @@
 const CreatePage = () => {};
-return <></>;
+return (
+  <div>
+    <div>
+      <input id="registeremail-textfield" type="email" placeholder="Email" />
+    </div>
+    <div>
+      <input id="registerpassword-textfield" type="password" placeholder="Email" />
+    </div>
+    <div>
+      <input id="confirmpassword-textfield" type="password" placeholder="Email" />
+    </div>
+    <div>
+      <button id="registeraccount-button"></button>
+    </div>
+    <div>
+      <button id="registercancel-button"></button>
+    </div>
+  </div>
+);
 
 export default CreatePage;
