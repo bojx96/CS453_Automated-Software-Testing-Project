c13593fb8bb7345265c125b663080665f77ea4be
Han Wei Guang <hanweiguang25@gmail.com>
2023-05-29 18:39:28 +0900

    added input.py into tester, updated txt

--- tester.py
+++ tester.py
@@ -1,3 +1,4 @@
+import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,6 +7,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
 search_list = ["input", "button"]
+parser = argparse.ArgumentParser("Input")
+
+# add the arguments
+parser.add_argument('-p', '--path', type=str, required=True)
+parser.add_argument('-u', '--url', type=str, required=True)
+
+# get the inputs
+args = parser.parse_args()
 
 def get_elements(soup, name):
     return soup.find_all(name)
@@ -21,33 +30,35 @@ def parsing_elements(filename):
             resulted_list.append((el, obj['id']))
     return resulted_list
 
-driver = webdriver.Chrome()
-driver.get("http://www.google.com") # change to local host
+# do sth to the inputs
+if args.path and args.url:
+    elements = parsing_elements(args.path)
+    driver = webdriver.Chrome()
+    print(args.url)
+    driver.get(args.url)
+    test_cases = []
+    for element in elements:
+        test_cases.append('F')
 
-elements = [("div","SIvCob"), ("textarea","APjFqb"), ("div","123")] # Dictionary of tag_name:id, eg button:login-button
+    test_counter = 0
+    for tup in elements:
+        tag_type, id = tup[0], tup[1]
+        try:
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+                (By.ID, id)))  # Making sure website is loaded first
+            elem = driver.find_element(By.ID, id)
+            # if elem.tag_name != tag_type:
+            #     raise Exception("tag_name of element is not the same as defined element!")
+            # else:
+            #     test_cases[test_counter] = 'T'
+            test_cases[test_counter] = 'T'
+            test_counter += 1
+        except Exception as e:
+            print(f"Exception Occured: {e}")
 
-test_cases = []
-for element in elements:
-    test_cases.append('F')
-
-test_counter = 0
-for tup in elements:
-    tag_type,id = tup[0],tup[1]
-    try:
-        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID,id))) # Making sure website is loaded first
-        elem = driver.find_element(By.ID,id)
-        # if elem.tag_name != tag_type:
-        #     raise Exception("tag_name of element is not the same as defined element!")
-        # else:
-        #     test_cases[test_counter] = 'T'
-        test_cases[test_counter] = 'T'
-        test_counter += 1
-    except Exception as e:
-        print(f"Exception Occured: {e}")
-
-for bool1 in test_cases:
-    if bool1 == 'T':
-        print("\033[92mP", end="")
-    else:
-        print("\033[91mF", end="")
-print("\033[0m")
+    for bool1 in test_cases:
+        if bool1 == 'T':
+            print("\033[92mP", end="")
+        else:
+            print("\033[91mF", end="")
+    print("\033[0m")
