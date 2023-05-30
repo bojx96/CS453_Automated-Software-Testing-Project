8692040eed1098152fe81c600902231004691d0e
bojx96 <brandon2_ong@mymail.sutd.edu.sg>
2023-05-30 16:31:19 +0800

    feat: settingpage styling

--- frontend/test-app/src/pages/SettingsPage.js
+++ frontend/test-app/src/pages/SettingsPage.js
@@ -2,11 +2,13 @@ import "../App.css";
 const SettingsPage = () => {
   return (
     <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column ", height: "100%" }}>
-      <div style={{ padding: 32, paddingTop: 150 }}>
-        <input id="changepassword-textfield" class="Text-box" placeholder="Change Password *" />
+      <div style={{ padding: 32, paddingTop: 150, display: "flex", flexDirection: "column" }}>
+        <text class="Text-box-char">Change Password</text>
+        <input id="changepassword-textfield" class="Text-box" required />
       </div>
-      <div>
-        <input id="confirmchange-textfield" class="Text-box" type="text" placeholder="Confirm Password *" />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <text class="Text-box-char">Confirm Password</text>
+        <input id="confirmchange-textfield" class="Text-box" type="text" required />
       </div>
       <div style={{ padding: 32 }}>
         <button id="changesubmit-button" class="Primary-button">
