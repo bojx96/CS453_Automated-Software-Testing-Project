5e4dc6525e30c8249c18242964518010dbe5c384
bojx96 <brandon2_ong@mymail.sutd.edu.sg>
2023-05-30 16:24:49 +0800

    feat: taskbar

--- frontend/test-app/src/App.js
+++ frontend/test-app/src/App.js
@@ -1,17 +1,55 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import cookieLogo from "./img/cookieLogo.png";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
 import CreatePage from "./pages/CreatePage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
+  const location = useLocation();
   return (
-    <BrowserRouter>
+    <>
+      {location.pathname != "/" && location.pathname != "/create" ? (
+        <div>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <div>
+              <img id="cookielogo" src={cookieLogo} alt="cookieLogo" style={{ paddingLeft: 56, paddingTop: 17, paddingBottom: 17, height: 68, width: 68 }} />
+            </div>
+            <div style={{ display: "flex", flexDirection: "row", alignItems: "end" }}>
+              <div style={{ paddingLeft: 88, paddingRight: 198 }}>
+                <button id="home-button" class="bar-button">
+                  Home
+                </button>
+              </div>
+              <div style={{ paddingRight: 198 }}>
+                <button id="mail-button" class="bar-button">
+                  Mail
+                </button>
+              </div>
+              <div>
+                <button id="settings-button" class="bar-button" style={{ paddingRight: 530 }}>
+                  Settings
+                </button>
+              </div>
+              <div>
+                <button id="signout-button" class="bar-button">
+                  Signout
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="line"></div>
+        </div>
+      ) : (
+        <></>
+      )}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/create" element={<CreatePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
